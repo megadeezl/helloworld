1dff6eb31475fde819949663c5590ef98b84bb0d
erobin08 <Eric_Robinson3@cable.comcast.com>
2016-01-20 10:26:50 -0500

    Testing webhook

--- hello.go
+++ hello.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/megadeezl/stringutil"
 )
 
 func main() {
-  //fmt.Printf("Hello, world.\n")
-  fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+  fmt.Printf("Hello, world.\n")
   fmt.Println("\n")
 }
 
